ca631b1e773d467c1c25164946eedbbba5063764
Djesse Dirckx <djessedirckx@gmail.com>
2019-11-25 10:46:48 +0100

    Training test split test

--- Exploration/binary_data.py
+++ Exploration/binary_data.py
@@ -1,45 +1,44 @@
 import pandas as pd
 import numpy as np
-import time
 
-# Read training data
-training_file_path = "../Data/train.json"
-data = pd.read_json(training_file_path, orient='columns').head(500)
+def convert_to_binary(data):
 
-# Extract X (ingredients)
-X = data['ingredients']
+    # Extract X (ingredients)
+    X = data['ingredients']
 
-# Create a unique set of ingredients
-ingredient_set = set()
-for ingredients in X:
-    for ingredient in ingredients:
-        ingredient_set.add(ingredient)
+    # Create a unique set of ingredients
+    ingredient_set = set()
+    for ingredients in X:
+        for ingredient in ingredients:
+            ingredient_set.add(ingredient)
+            
+    # Convert set to list to create indexes
+    ingredients_list = list(ingredient_set)
+
+    # Add a column for every ingredient to the dataframe
+    i = 0
+    for ingredient in ingredients_list:
+        i = i + 1
         
-# Convert set to list to create indexes
-ingredients_list = list(ingredient_set)
+        if i % 1000 ==0:
+            print(i)
+        scores = []
+        
+        # Check whether recipe contains ingredient or not
+        for rec_ing in data['ingredients']:
+            if ingredient in rec_ing:
+                scores.append(1)
+            else:
+                scores.append(0)
+        
+        data[ingredient] = scores
 
-# Add a column for every ingredient to the dataframe
-i = 0
-for ingredient in ingredients_list:
-    i = i + 1
-    
-    if i % 1000 ==0:
-        print(i)
-    scores = []
-    
-    # Check whether recipe contains ingredient or not
-    for rec_ing in data['ingredients']:
-        if ingredient in rec_ing:
-            scores.append(1)
-        else:
-            scores.append(0)
-    
-    data[ingredient] = scores
+    # Drop ingredients column
+    print('Dropping ingredients column')
+    data = data.drop(['ingredients'],axis=1)
 
-# Drop ingredients column
-data = data.drop(['ingredients'],axis=1)
+    # Write file to csv
+    print('Writing data to csv')
+    data.to_csv(r'training_binary.csv')
 
-# Write file to csv
-data.to_csv(r'training_binary.csv')
-
-print(len(data.columns))
+    print(len(data.columns))
